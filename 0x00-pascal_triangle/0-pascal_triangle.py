c0bc29bad930da7ed0b7b43f497fd12c27c4a80c
josfam <jamukun@proton.me>
2024-07-28 23:40:44 +0300

    Fix: Do not import any module
    The sliding window (the deque) has been manually implemented by a list

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,21 +2,22 @@
 
 """Pascal's triangle interview question solution"""
 
-from collections import deque
-
 def pascal_triangle(n):
     """returns a list of lists of integers representing the Pascal’s triangle of n"""
     if n <= 0:
         return []
 
-    triangle = [[1]]
+    MAX_WINDOW_SIZE = 2
     ROWS_MADE = 1
-    pairs = deque(maxlen=2) # sliding window
+    triangle = [[1]]
+    pairs = [] # sliding window
 
     for _ in range(ROWS_MADE, n):
         last_row = triangle[-1]
         new_row = []  # new row to add to the triangle
         for num in last_row:
+            if len(pairs) == MAX_WINDOW_SIZE:
+                pairs.pop(0)
             pairs.append(num)
             new_row.append(sum(pairs))
         new_row.append(1) # add the 1 in the last position
