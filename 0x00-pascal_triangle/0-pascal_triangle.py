85a015d9ba7b0cd5316d23091821ad1d45c229f5
josfam <jamukun@proton.me>
2024-07-28 23:23:44 +0300

    Fix: User <= instead of ==

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,7 +5,7 @@ from collections import deque
 
 def pascal_triangle(n):
     """returns a list of lists of integers representing the Pascal’s triangle of n"""
-    if n == 0:
+    if n <= 0:
         return []
 
     triangle = [[1]]
