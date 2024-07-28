046c6137da9dba62e6e6d8845e15fd0505af8a02
josfam <jamukun@proton.me>
2024-07-28 23:42:32 +0300

    Fix formatting errors

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,15 +2,17 @@
 
 """Pascal's triangle interview question solution"""
 
+
 def pascal_triangle(n):
-    """returns a list of lists of integers representing the Pascal’s triangle of n"""
+    """Returns a list of lists of integers representing
+    the Pascal’s triangle of n rows"""
     if n <= 0:
         return []
 
     MAX_WINDOW_SIZE = 2
     ROWS_MADE = 1
     triangle = [[1]]
-    pairs = [] # sliding window
+    pairs = []  # sliding window
 
     for _ in range(ROWS_MADE, n):
         last_row = triangle[-1]
@@ -20,7 +22,7 @@ def pascal_triangle(n):
                 pairs.pop(0)
             pairs.append(num)
             new_row.append(sum(pairs))
-        new_row.append(1) # add the 1 in the last position
+        new_row.append(1)  # add the 1 in the last position
         triangle.append(new_row)
         pairs.clear()
 
