9b88cdb0e40e1e0b6cbaf47b70f53ded434e65a1
josfam <josephamukun@tutanota.com>
2024-08-16 01:54:32 +0000

    Only show status codes that appeared
    Status codes with a count of 0 do not appear in the summary

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -32,7 +32,8 @@ def print_summary() -> None:
     """
     print(f'File size: {total_file_size}')
     for status_code, count in status_counts.items():
-        print(f'{status_code}: {count}')
+        if count > 0:
+            print(f'{status_code}: {count}')
 
 
 try:
