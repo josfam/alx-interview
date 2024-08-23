2771f230d98e531f29cf316e29e16c60c0fb87e7
josfam <jamukun@proton.me>
2024-08-23 05:53:17 +0300

    Implement `0. UTF-8 Validation`

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+"""Contains function that determines whether the provided data represents
+valid UTF-8 encoding"""
+
+
+def validUTF8(data):
+    """Determines whether the provided data represents UTF-8 encoding"""
+    MAX_BITS = 8
+    padded = [str(bin(num)).lstrip('0b').zfill(8) for num in data]
+
+    is_multibyte = False
+    cont_bytes_expected = 0
+    cont_bytes_got = 0
+    i = 0
+
+    while i < len(padded):
+        num = padded[i]
+
+        if len(num) > MAX_BITS:  # more than 8 bits is too many
+            return False
+        if num.startswith('0'):  # normal ASCII letter
+            pass
+        elif num.startswith('11'):  # multibyte
+            cont_bytes_expected = num.find('0')
+            # look at the next cont_bytes_expected numbers
+            cont_bytes = padded[i + 1 : i + cont_bytes_expected + 1]
+            i += 1
+            for cont_byte in cont_bytes:
+                if not cont_byte.startswith('10'):  # illegal continuation
+                    return False
+                cont_bytes_got += 1
+                i += 1
+        else:  # starts with 10 (misplaced continuation byte)
+            return False
+        i += 1
+
+    return True
