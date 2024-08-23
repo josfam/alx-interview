048820f4be7d66a68fe242097223f6bb0d947557
josfam <jamukun@proton.me>
2024-08-23 06:07:01 +0300

    Remove magic number & make comments clearer

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,9 +7,9 @@ valid UTF-8 encoding"""
 def validUTF8(data):
     """Determines whether the provided data represents UTF-8 encoding"""
     MAX_BITS = 8
+    MAX_CONT_BYTES = 6
     padded = [str(bin(num)).lstrip('0b').zfill(8) for num in data]
 
-    is_multibyte = False
     cont_bytes_expected = 0
     cont_bytes_got = 0
     i = 0
@@ -17,15 +17,15 @@ def validUTF8(data):
     while i < len(padded):
         num = padded[i]
 
-        if len(num) > MAX_BITS:  # more than 8 bits is too many
+        if len(num) > MAX_BITS:
             return False
         if num.startswith('0'):  # normal ASCII letter
             pass
         elif num.startswith('11'):  # multibyte
             cont_bytes_expected = num.find('0')
-            if cont_bytes_expected > 6:  # max possible
+            if cont_bytes_expected > MAX_CONT_BYTES:
                 return False
-            # look at the next cont_bytes_expected numbers
+            # check expected continuation bytes
             cont_bytes = padded[i + 1: i + cont_bytes_expected + 1]
             i += 1
             for cont_byte in cont_bytes:
