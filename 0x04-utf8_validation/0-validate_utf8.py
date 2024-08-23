2b9d3d9db43b354cfccf0f284eb6a685a3db6868
josfam <jamukun@proton.me>
2024-08-23 16:36:25 +0300

    Fix: Consider that a UTF-8 char can be 4 bytes
    At most, 3 continuation bytes should be exptected.
    
    Refactor: User all() idiom to check if all continuation bytes are
    actually valid
    Refactor: Rename `cont_bytes_after` to `cont_bytes_expected` for more
    clarity

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,14 +7,13 @@ valid UTF-8 encoding"""
 def validUTF8(data):
     """Determines whether the provided data represents UTF-8 encoding"""
     MAX_BITS = 8
-    MAX_CONT_BYTES = 6
+    MAX_CONT_BYTES = 3
     padded = [
         str(bin(num)).lstrip('0b')[-MAX_BITS:].zfill(MAX_BITS)
         for num in data
     ]
 
-    cont_bytes_after = 0
-    cont_bytes_got = 0
+    cont_bytes_expected = 0
     i = 0
 
     while i < len(padded):
@@ -23,22 +22,22 @@ def validUTF8(data):
         if num.startswith('0'):  # normal ASCII letter
             pass
         elif num.startswith('11'):  # multibyte
-            cont_bytes_after = num.find('0') - 1
-            if cont_bytes_after > MAX_CONT_BYTES:
+            cont_bytes_expected = num.find('0') - 1
+            if cont_bytes_expected > MAX_CONT_BYTES:
                 return False
             # check expected continuation bytes
-            cont_bytes = padded[i + 1: i + cont_bytes_after + 1]
-            if not cont_bytes:  # no more bytes exist
+            cont_bytes = padded[i + 1: i + cont_bytes_expected + 1]
+            valid_conts = [
+                1 if byte.startswith('10') else 0
+                for byte in cont_bytes
+            ]
+
+            if not cont_bytes \
+                or not all(valid_conts) \
+                    or not len(valid_conts) == cont_bytes_expected:
                 return False
-            i += 1  # move to the first continuation byte
-            for cont_byte in cont_bytes:
-                if not cont_byte.startswith('10'):  # illegal continuation
-                    return False
-                cont_bytes_got += 1
-                i += 1
-            if not cont_bytes_got == cont_bytes_after:
-                return False
-            cont_bytes_got = 0
+
+            i += len(valid_conts)  # skip to the last continuation byte
         else:  # starts with 10 (misplaced continuation byte)
             return False
         i += 1
