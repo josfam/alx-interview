6858a199472124895295e4babc17ac87f1e33dd3
josfam <jamukun@proton.me>
2024-09-11 18:14:33 +0300

    Fix black formatting

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,6 +2,7 @@
 
 """Rotates a 2D n x n matrix in-place, by 90 degrees clockwise"""
 
+
 def rotate_2d_matrix(matrix):
     """Rotates an n x n 2D matrix in-place, by 90 degrees clockwise.
     For this, it is assumed that the matrix will have 2 dimensions, and is not
