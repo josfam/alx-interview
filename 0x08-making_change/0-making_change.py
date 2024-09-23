8da4cf505f3d3ca6fae1c0835cf921aa175c31a0
josfam <jamukun@proton.me>
2024-09-23 19:01:34 +0300

    Implement `0. Change comes from within`

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""Determines the fewest number of coins needed to meet a given amount total
+using a greedy algorithm"""
+
+import time
+
+
+def makeChange(coins, total):
+    """
+    Args:
+        coins: A list of values of coins in our possession. It is assumed that
+        there is an infinite number of each coin
+        total: The total amount of change to get by combining the coins in our
+        possession.
+    Returns:
+        The fewest number of coins needed to meet total
+    """
+    min_coins = 0
+    sorted_coins = list(reversed(sorted(coins)))
+    print(f'{sorted_coins=} {total=}')
+    for coin in sorted_coins:
+        while total >= coin:
+            print('new loop ===')
+            min_coins += total // coin
+            total = total % coin
+            print(f'{total=}, {coin=}, total // coin = {total // coin}')
+            time.sleep(1)
+
+    if total != 0:  # not all coins could be divided
+        return -1
+    return min_coins
